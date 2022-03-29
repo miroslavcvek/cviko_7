743ef61b41d6fb8b0fb551b0c6b35113733768e1
Miroslav Cvek <xcvekm00@vutbr.cz>
2022-03-29 09:50:26 +0200

    Add check for whole numbers.

---
+++ analyse_numbers.py
@@ -0,0 +1,13 @@
+def is_whole_number(number):
+    if float(number) == True:
+        message = "Neni cele cislo"
+        print(message)
+
+    else:
+
+        mess = "Cislo je cele"
+        print(mess)
+
+
+cislo = is_whole_number(4)
+print(cislo)
